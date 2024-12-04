bf64ed65c1393a096d114f27f9c25c8b165b2f8f
Nagai_sho <as.uland.dr@gmail.com>
2024-12-04 16:35:03 +0900

    newアクションの追加

--- app/controllers/qrcodes_controller.rb
+++ app/controllers/qrcodes_controller.rb
@@ -2,10 +2,15 @@ class QrcodesController < ApplicationController
   require 'rqrcode'
   require 'chunky_png'
 
+  def new
+    @qrcode = Qrcode.new
+  end
+
   def create
     @qrcode = Qrcode.new(qrcode_params)
     if @qrcode.save
       generate_qr_code(@qrcode)
+      # url_for(@qrcode): @qrcodeオブジェクトに対応するURLを生成するRailsのヘルパーメソッド
       render json: { success: true, qr_code_url: url_for(@qrcode) }
     else
       render json: { success: false, errors: @qrcode.errors.full_messages }
@@ -46,11 +51,17 @@ class QrcodesController < ApplicationController
     qr_code.update(file_name: file_path.to_s)
   end
 
+  # 与えられたURLを暗号化し、Base64 URLセーフエンコーディングを行う機能
   def encrypt_url(url)
+    # 暗号化アルゴリズム（AES-256-CBC）を使用するOpenSSL::Cipherオブジェクトを作成
     cipher = OpenSSL::Cipher.new('AES-256-CBC')
+    # 暗号化モードに設定
     cipher.encrypt
+    # 環境変数から暗号化キーを取得し、cipherオブジェクトに設定
     cipher.key = ENV['ENCRYPTION_KEY']
+    # cipher.updateメソッドでURLを暗号化し、cipher.finalで暗号化処理を完了させる。
     encrypted = cipher.update(url) + cipher.final
+    # 暗号化されたデータをBase64 URLセーフエンコーディングする。
     Base64.urlsafe_encode64(encrypted)
   end
 end
